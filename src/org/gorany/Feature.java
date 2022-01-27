e8f57bdb4db62bc87570148cb9bbf396f866b815
poby-kr <jhkim07@trenbe.com>
2022-01-28 01:50:58 +0900

    [main] First Snapshot

---
+++ src/org/gorany/Feature.java
@@ -0,0 +1,8 @@
+package org.gorany;
+
+public class Feature {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/org/gorany/Main.java
@@ -0,0 +1,8 @@
+package org.gorany;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
