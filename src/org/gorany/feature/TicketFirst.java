75d5f112f6ab813e7b9f3e85fe08cbd14195f4a9
poby-kr <jhkim07@trenbe.com>
2022-01-30 17:09:37 +0900

    [Rebase] commit 3 - ticket-3

--- src/org/gorany/feature/TicketFirst.java
+++ src/org/gorany/feature/TicketFirst.java
@@ -5,5 +5,7 @@ public class TicketFirst {
     public static void main(String[] args) {
         /* Commit 1 - ticket-1 */
         /* Commit 2 - ticket-2 */
+
+        /* Commit 3 - ticket-3 */
     }
 }
