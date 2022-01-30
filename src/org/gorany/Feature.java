a18417eef023e4a98399a23943d8548132e76f9d
poby-kr <jhkim07@trenbe.com>
2022-01-30 16:34:24 +0900

    [Rebase] commit 4 - rebase/1 (final squash)
    [Rebase] commit 5 - rebase/3 (squash!)
    
    [Rebase] commit 6 - rebase/3 (squash)
    
    [Rebase] commit 7 - rebase/3
    
    [Rebase] commit 8 - rebase/3 (squash)

--- src/org/gorany/Feature.java
+++ src/org/gorany/Feature.java
@@ -7,5 +7,9 @@ public class Feature {
         /* Commit 2 - rebase/2 */
         /* Commit 3 - rebase/2 */
         /* Commit 4 - rebase/1 */
+        /* Commit 5 - rebase/3 */
+        /* Commit 6 - rebase/3 */
+        /* Commit 7 - rebase/3 */
+        /* Commit 8 - rebase/3 */
     }
 }
