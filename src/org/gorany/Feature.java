40034ec4a817ca370d3b6fa3664f3490c198bafd
poby-kr <jhkim07@trenbe.com>
2022-01-31 15:39:40 +0900

    [Rebase] rebase-1 (3)

--- src/org/gorany/Feature.java
+++ src/org/gorany/Feature.java
@@ -5,5 +5,6 @@ public class Feature {
     public static void main(String[] args) {
         /* feature/rebase-1 (1) */
         /* feature/rebase-1 (2) */
+        /* feature/rebase-1 (3) */
     }
 }
